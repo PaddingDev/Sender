213a95f4a8c9a917dc7c884beb8aed8d44d22d0c
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2022-10-26 18:07:00 +0100

    feat: get info

---
+++ src/module/function/storage/getInfo.go
@@ -0,0 +1,58 @@
+package storage
+
+import (
+	"github.com/PaddingDEV/Sender/module/function/storage/model"
+	"github.com/PaddingDEV/Sender/utils"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"time"
+)
+
+func GetFileInfoHandler(c *gin.Context) {
+	uuid := c.GetString("uuid")
+	token := c.GetString("token")
+	if uuid == "" {
+		utils.HttpReturnWithErrAndAbort(
+			c,
+			http.StatusBadRequest,
+			"need uuid")
+		return
+	}
+	path := getFilePath(uuid)
+	if !isPathExists(path) {
+		utils.HttpReturnWithErrAndAbort(
+			c,
+			http.StatusBadRequest,
+			"No file")
+	}
+	infoBs, err := utils.ReadFileToByte(path + "info.json")
+	if err != nil {
+		utils.HttpReturnWithErrAndAbort(
+			c,
+			http.StatusBadRequest,
+			"no info")
+	}
+	info := model.FileInfo{}
+	err = utils.FromJsonTo(infoBs, &info)
+	if err != nil {
+		utils.HttpReturnWithErrAndAbort(
+			c,
+			http.StatusBadRequest,
+			"failed to load info")
+	}
+	if time.Now().After(info.ExpiredAt) {
+		utils.HttpReturnWithErrAndAbort(
+			c,
+			http.StatusBadRequest,
+			"expired!")
+		_ = utils.RemoveFile(getFileStorePath(uuid))
+	}
+	if info.Token != token {
+		utils.HttpReturnWithErrAndAbort(
+			c,
+			http.StatusUnauthorized,
+			"Wrong token")
+	}
+	info.Token = ""
+	c.JSON(http.StatusOK, info)
+}
